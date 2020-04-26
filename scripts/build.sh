ef5b345efe923b942a7436d88ee495c2cf057a2b
William Martín Chávez González <ingmartic@gmail.com>
2020-04-25 19:59:11 -0500

    Before_script option included in travis.yml in order to run mvn inside the prject folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
-cd WMC-DevOps-EducationI/01-reference-architecture-springboot-classroom-teama/tree/feature/spring_boot_reference_architecture_description/SpiringBoot-ReferenceArchitecture
+cd $TRAVIS_BUILD_DIRWMC-DevOps-EducationI/01-reference-architecture-springboot-classroom-teama/tree/feature/spring_boot_reference_architecture_description/SpringBoot-ReferenceArchitecture
 mvn clean install
